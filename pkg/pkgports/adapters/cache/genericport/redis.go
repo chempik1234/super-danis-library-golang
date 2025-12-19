2dd4544e9f7c18998cf5abf169ae992110f98d95
Danis <dandijar@yandex.ru>
2025-12-19 14:45:16 +0500

    feat: redis cache TTL

--- pkg/pkgports/adapters/cache/genericport/redis.go
+++ pkg/pkgports/adapters/cache/genericport/redis.go
@@ -6,21 +6,23 @@ import (
 	"fmt"
 	"github.com/chempik1234/super-danis-library-golang/pkg/genericports"
 	"github.com/go-redis/redis/v8"
+	"time"
 )
 
 // RedisGenericCache - implement genericports.GenericCachePort
 type RedisGenericCache[K comparable, V genericports.ObjectWithIdentifier[K]] struct {
 	client *redis.Client
+	ttl    time.Duration
 }
 
 // NewRedisGenericCache creates a new instance of RedisGenericCache
-func NewRedisGenericCache[K comparable, V genericports.ObjectWithIdentifier[K]](addr string, password string, db int) *RedisGenericCache[K, V] {
+func NewRedisGenericCache[K comparable, V genericports.ObjectWithIdentifier[K]](addr string, password string, db int, ttlMs int) *RedisGenericCache[K, V] {
 	client := redis.NewClient(&redis.Options{
 		Addr:     addr,
 		Password: password,
 		DB:       db,
 	})
-	return &RedisGenericCache[K, V]{client: client}
+	return &RedisGenericCache[K, V]{client: client, ttl: time.Duration(ttlMs) * time.Millisecond}
 }
 
 // GetObjectByID - impl genericports.GenericCachePort.GetObjectByID
@@ -50,7 +52,7 @@ func (s *RedisGenericCache[K, V]) SaveObject(ctx context.Context, fullyReadyObje
 		return nil, err
 	}
 
-	if err := s.client.Set(ctx, key, data, 0).Err(); err != nil {
+	if err := s.client.Set(ctx, key, data, s.ttl).Err(); err != nil {
 		return nil, err
 	}
 
