dabaa823f37ffe731d318ec6137260ece8cdde3b
Danis <dandijar@yandex.ru>
2025-12-28 15:33:07 +0500

    fix: config fields yaml naming camel -> snake (mongo)

--- pkg/mongodb/mongodb.go
+++ pkg/mongodb/mongodb.go
@@ -12,12 +12,15 @@ import (
 // Config - required to connect to MongoDB with this library
 type Config struct {
 	Hosts       []string `env:"HOSTS" yaml:"hosts" envDefault:"mongodb:27017"`
-	MinPoolSize uint64   `env:"MIN_POOL_SIZE" yaml:"minPoolSize" envDefault:"1"`
-	MaxPoolSize uint64   `env:"MAX_POOL_SIZE" yaml:"maxPoolSize" envDefault:"10"`
+	MinPoolSize uint64   `env:"MIN_POOL_SIZE" yaml:"min_pool_size" envDefault:"1"`
+	MaxPoolSize uint64   `env:"MAX_POOL_SIZE" yaml:"max_pool_size" envDefault:"10"`
 
 	UserName    string `env:"USERNAME" yaml:"username" envDefault:"root"`
 	Password    string `env:"PASSWORD" yaml:"password" envDefault:"root"`
-	PasswordSet bool   `env:"PASSWORD_SET" yaml:"passwordSet" envDefault:"false"`
+	PasswordSet bool   `env:"PASSWORD_SET" yaml:"password_set" envDefault:"false"`
+
+	RetryWrites bool `env:"RETRY_WRITES" yaml:"retry_writes" envDefault:"true"`
+	RetryReads  bool `env:"RETRY_READS" yaml:"retry_reads" envDefault:"true"`
 }
 
 // New - connect to MongoDB with given config
@@ -40,6 +43,8 @@ func New(ctx context.Context, config Config) (*mongo.Client, error) {
 			Password:    config.Password,
 			PasswordSet: config.PasswordSet,
 		}),
+		options.Client().SetRetryWrites(config.RetryWrites),
+		options.Client().SetRetryReads(config.RetryReads),
 	)
 	if err != nil {
 		return nil, fmt.Errorf("failed to connect to mongodb: %w", err)
