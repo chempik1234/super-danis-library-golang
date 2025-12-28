e6187c797d95bc870ee1790b9a1d9bd11d590197
Danis <dandijar@yandex.ru>
2025-12-28 14:21:06 +0500

    feat: mongodb

--- go.mod
+++ go.mod
@@ -16,12 +16,18 @@ require (
 require (
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/golang/snappy v1.0.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
-	github.com/klauspost/compress v1.15.11 // indirect
+	github.com/klauspost/compress v1.16.7 // indirect
 	github.com/lib/pq v1.10.9 // indirect
 	github.com/pierrec/lz4/v4 v4.1.16 // indirect
+	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
+	github.com/xdg-go/scram v1.1.2 // indirect
+	github.com/xdg-go/stringprep v1.0.4 // indirect
+	github.com/youmark/pkcs8 v0.0.0-20240726163527-a2c0da244d78 // indirect
+	go.mongodb.org/mongo-driver/v2 v2.4.1 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	golang.org/x/crypto v0.45.0 // indirect
 	golang.org/x/net v0.47.0 // indirect
---
+++ pkg/mongodb/mongodb.go
@@ -0,0 +1,66 @@
+package mongodb
+
+import (
+	"context"
+	"fmt"
+	"github.com/chempik1234/super-danis-library-golang/pkg/logger"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
+	"go.mongodb.org/mongo-driver/v2/mongo/readpref"
+)
+
+// Config - required to connect to MongoDB with this library
+type Config struct {
+	Hosts       []string `env:"HOSTS" yaml:"hosts" envDefault:"mongodb:27017"`
+	MinPoolSize uint64   `env:"MIN_POOL_SIZE" yaml:"minPoolSize" envDefault:"1"`
+	MaxPoolSize uint64   `env:"MAX_POOL_SIZE" yaml:"maxPoolSize" envDefault:"10"`
+
+	UserName    string `env:"USERNAME" yaml:"username" envDefault:"root"`
+	Password    string `env:"PASSWORD" yaml:"password" envDefault:"root"`
+	PasswordSet bool   `env:"PASSWORD_SET" yaml:"passwordSet" envDefault:"false"`
+}
+
+// New - connect to MongoDB with given config
+//
+// tries to PING after connecting
+//
+//	client, err := New(ctx, config, rc, wc)
+//	if err != nil {
+//	   logger.GetLoggerFromCtx(ctx).Error(ctx, "aw hell no")
+//	   return
+//	}
+//	defer DeferDisconnect(ctx, client)
+func New(ctx context.Context, config Config) (*mongo.Client, error) {
+	client, err := mongo.Connect(
+		options.Client().SetMinPoolSize(config.MinPoolSize),
+		options.Client().SetMaxPoolSize(config.MaxPoolSize),
+		options.Client().SetHosts(config.Hosts),
+		options.Client().SetAuth(options.Credential{
+			Username:    config.UserName,
+			Password:    config.Password,
+			PasswordSet: config.PasswordSet,
+		}),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to mongodb: %w", err)
+	}
+	err = client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		return nil, fmt.Errorf("failed to ping mongodb: %w", err)
+	}
+	return client, nil
+}
+
+// DeferDisconnect - call in defer after getting client.
+//
+//	client, err := New(ctx, config, rc, wc)
+//	if err != nil {
+//	   logger.GetLoggerFromCtx(ctx).Error(ctx, "aw hell no")
+//	   return
+//	}
+//	defer DeferDisconnect(ctx, client)
+func DeferDisconnect(ctx context.Context, client *mongo.Client) {
+	if err := client.Disconnect(ctx); err != nil {
+		logger.GetOrCreateLoggerFromCtx(ctx).Error(ctx, "failed to disconnect from mongodb client")
+	}
+}
